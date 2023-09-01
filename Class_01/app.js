85c841bba68772ca375f1ca6d6bb9b2a075c62f6
Abdul Samad <as765722@gmail.com>
2023-09-01 17:38:01 +0500

    Added Class_01

---
+++ Class_01/app.js
@@ -0,0 +1,10 @@
+// Basic Types
+// 1. String: string=> 'Apple', 'Orange';
+// 2. Numbger: number=> any number
+// 3. Boolean: boolean=> true,false
+var person = "samad";
+person = "Akram";
+var arr = ["a", "b", "c"];
+arr.push("pineapple");
+var arr2 = [1, 2, 3, 4, 5, 6];
+arr2.push(7);
---
+++ Class_01/app.ts
@@ -0,0 +1,15 @@
+// Basic Types
+
+// 1. String: string=> 'Apple', 'Orange';
+// 2. Numbger: number=> any number
+// 3. Boolean: boolean=> true,false
+
+let person = "samad";
+person = "Akram";
+
+const arr: string[] = ["a", "b", "c"];
+arr.push("pineapple");
+
+const arr2: number[] = [1, 2, 3, 4, 5, 6];
+arr2.push(7);
+
