0f6f1f6abfaae60ad1cdfc4afd0ce66c0db9a2d7
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2023-09-05 22:10:18 +0500

    Added Class_06

--- Class_06/dist/app.js
+++ Class_06/dist/app.js
@@ -5,13 +5,33 @@
 // const input2 = <HTMLInputElement>document.getElementById("Input"); // recommended way
 // Generics
 // to give types to promise for avoiding errors
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Task performed Successfully!");
-    }, 2000);
-});
-// if dont define promise type then it will give error on split method.
-promise.then((data) => {
-    console.log(data.split(" "));
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("Task performed Successfully!");
+//     }, 2000);
+// })
+// // if dont define promise type then it will give error on split method.
+// promise.then((data) => {
+//     console.log(data.split(" "));
+// })
+// type User = {
+//     name: string;
+//     class: number
+// }
+// const promise: Promise<User> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve({
+//             name: 'Samad',
+//             class: 10
+//         });
+//     }, 2000);
+// })
+// promise.then((data) => {
+//     console.log(data.name);
+// })
+function merge(objA, objB) {
+    return Object.assign(Object.assign({}, objA), { objB });
+}
+const merged = merge({ name: 'Samad' }, { id: 123 });
+console.log(merged.name);
 //# sourceMappingURL=app.js.map
--- Class_06/src/app.ts
+++ Class_06/src/app.ts
@@ -9,13 +9,39 @@
 // Generics
 // to give types to promise for avoiding errors
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Task performed Successfully!");
-    }, 2000);
-})
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("Task performed Successfully!");
+//     }, 2000);
+// })
 
-// if dont define promise type then it will give error on split method.
-promise.then((data) => {
-    console.log(data.split(" "));
-})
+// // if dont define promise type then it will give error on split method.
+// promise.then((data) => {
+//     console.log(data.split(" "));
+// })
+
+// type User = {
+//     name: string;
+//     class: number
+// }
+
+// const promise: Promise<User> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve({
+//             name: 'Samad',
+//             class: 10
+//         });
+//     }, 2000);
+// })
+
+
+// promise.then((data) => {
+//     console.log(data.name);
+// })
+
+function merge<T extends {}, U extends {}>(objA: T, objB: U) {
+    return { ...objA, objB };
+}
+
+const merged = merge({ name: 'Samad' }, { id: 123 });
+console.log(merged.name)
